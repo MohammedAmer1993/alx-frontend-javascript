c84d67815c7cd511a55bc0b4641286a8c23eeb09
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-01 02:45:29 +0300

    task 10 project ecma basics

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const value of array) {
+    const newarr = [];
+    newarr.push(`${appendString} + ${value}`);
+  }
+
+  return array;
+}
